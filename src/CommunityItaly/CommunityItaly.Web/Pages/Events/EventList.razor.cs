0a5157b9b9b25ca624593f8d69761a4413d4621d
Andrea Tosato <andrea.tosato@hotmail.it>
2020-05-02 17:34:38 +0200

    Fix Snackbar

--- src/CommunityItaly/CommunityItaly.Web/Pages/Events/EventList.razor.cs
+++ src/CommunityItaly/CommunityItaly.Web/Pages/Events/EventList.razor.cs
@@ -18,18 +18,6 @@ namespace CommunityItaly.Web.Pages.Events
 		public int PageIndex { get; set; } = 1;
 		public int Total { get; set; }
 
-		//protected override async Task OnInitializedAsync()
-		//{
-		//	//await LoadDataAsync().ConfigureAwait(false);
-		//}
-
-		//async Task OnPage(MatPaginatorPageEvent e)
-		//{
-		//	PageSize = e.PageSize;
-		//	PageIndex = e.PageIndex;
-		//	await LoadDataAsync().ConfigureAwait(false);
-		//}
-
 		void Edit(EventViewModelReadOnly args)
 		{
 			AppStore.EventEdit = args;
@@ -46,10 +34,6 @@ namespace CommunityItaly.Web.Pages.Events
 			var pagedViewModel = await Http.GetEventsConfirmed(PageSize, PageSize * (PageIndex - 1)).ConfigureAwait(false);
 			EventViewModels = pagedViewModel.Entities;
 			Total = pagedViewModel.Total;
-
-			AppStore.AddNotification(new NotificationMessage("Prova", NotificationMessage.MessageType.Success));
-			AppStore.AddNotification(new NotificationMessage("Start", NotificationMessage.MessageType.Success));
-			StateHasChanged();
 		}
 
 		void Create()
